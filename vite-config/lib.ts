17f23a502da6363c61a4b7ab80c34d35b4b8c8bf
Gao Sheng <gaosheng08@meituan.com>
2022-06-08 19:56:33 +0800

    init

--- vite-config/lib.ts
+++ vite-config/lib.ts
@@ -13,7 +13,7 @@ export default mergeConfig(baseConfig, defineConfig({
         },
         rollupOptions: {
             // 确保外部化处理那些你不想打包进库的依赖
-            external: ['vue', 'element-plus', '@sheng/vnode-render', '@element-plus/icons-vue'],
+            external: ['vue', 'element-plus'],
             output: {
                 // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
                 globals: {
